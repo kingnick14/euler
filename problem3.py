be479e704cc66e8f84b61a4b231d68fb9b7a752e
King Nick <nar@marketlogicsoftware.com>
2018-07-25 10:33:24 +0200

    sdsdsd

--- problem3.py
+++ problem3.py
@@ -1,17 +1,24 @@
 inp = int(input('Enter a number:'))
-digits = [2,3,4,5,6,7,8,9,10,11,13,17,19,23,29,31,37]
+fulllist = []
 lst = []
 #rangelist = list(range(0,inp))
+
+
+###### check for the square root, all facotrs have to be below or above this number
+###### check every int below the square, and check for its mirror, if they are primes
+###### create a function that does the above check, and call it whenever you get a factors
+###### profit
+
 nextf = int(inp) - 1
 while nextf > 1:
-    #fulllist.append(nextf)
+    fulllist.append(nextf)
     rem = int(inp) % nextf
     if not rem == 0:
         nextf = nextf - 1
         continue
     else:
-        for i in digits:
-            if (nextf % i) == 0:
+        for i in fulllist:
+            if  (nextf % i) == 0:
                 lst.append(nextf)
                 nextf = nextf - 1
                 continue
