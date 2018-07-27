915854b6d10576549163e53d961d00ff8f79ccf8
King Nick <nar@marketlogicsoftware.com>
2018-07-27 18:32:25 +0200

    sdsd

--- problem3.py
+++ problem3.py
@@ -1,7 +1,23 @@
+import math
 inp = int(input('Enter a number:'))
+factors = []
+mirrors = []
+storeme = 0
 fulllist = []
-lst = []
+primes = []
 #rangelist = list(range(0,inp))
+def isprime(no, rangetop):
+    primelist = []
+    holder = []
+    del primelist [:]
+    del holder[:]
+    print(rangetop)
+    checker = range(1,int(math.sqrt(rangetop)+1))
+    for a in checker:
+        #print('debug',rangetop % a, ' is remainder of',a)
+        if rangetop % a == 0:
+            holder.append(a)
+    if len(holder) == 1: return True
 
 
 ###### check for the square root, all facotrs have to be below or above this number
@@ -10,16 +26,42 @@ lst = []
 ###### profit
 
 
+### find SquareRoot of inp
+rangelist = list(range(2,int(math.sqrt(inp)+1)))
+print('#s below SquareRoot:',rangelist)
+### search for factors up to SquareRoot of Inp
+for i in rangelist:
+    if not inp % i == 0: continue
+    factors.append(i)
+print('factors:',factors)
 
+### find mirrors of factors
+for x in factors:
+    storeme = int(inp / x)
+    mirrors.append(storeme)
+print('mirrors:', mirrors)
+### combine factors and mirrors
+fulllist = factors + mirrors
+print('Full list:',fulllist)
 
+for z in fulllist:
+    if isprime(z,z) == True:
+        primes.append(z)
+    #print('debug 2:', z)
 
+primes.sort()
+print('Your primes are', primes)
+### Check for primes bz dividing and storing factors and looking for len of 2
 
-
-
-
-
-
-
+# for y in fulllist:
+#     print(y)
+#     div = []
+#     cand = []
+#     div = list(range(1,y)
+#     for z in div:
+#         if z % y == 0
+#             cand.append(z)
+    #if len(cand) == 2: primes.append(y)
 
 # nextf = int(inp) - 1
 # while nextf > 1:
@@ -54,5 +96,5 @@ lst = []
 
 
 
-print (sum(lst))
-print(lst)
+#print (sum(lst))
+#print(lst)
