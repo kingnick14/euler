8230be3715052648a2a9a4a83f0be2bebe11377f
kingnick14 <40497055+kingnick14@users.noreply.github.com>
2018-07-29 19:56:21 +0200

    assasasasas

---
+++ problem4.py
@@ -0,0 +1,37 @@
+#def reverseEven(changeMe):
+    changed = changeMe[:1]
+
+import operator
+pal = 0
+count = 0
+firsthalf = 0
+secondhalf = 0
+numbers = range(100,999)
+
+for x in range(len(numbers)):
+    for y in range(x+1,len(numbers)):
+        multint = numbers[x]*numbers[y]
+        multstr = str(multint)
+        #print(len(multstr),'is the length of',multint)
+        count += 1
+
+        if len(multstr) % 2 == 0:
+            firsthalf = int(len(multstr)/2)
+
+            secondhalf = int(len(multstr)/2 * - 1)
+            if len(firsthalf/2) % 2 == 0
+                reversefirst =
+                #list comprehention
+
+
+
+
+
+            if multstr[:operator.__index__(firsthalf)] == multstr[operator.__index__(secondhalf):]:
+                pal = multint
+                print(pal)
+
+print(count,'combos found')
+print(pal)
+
+        #print(mult,type(mult))
